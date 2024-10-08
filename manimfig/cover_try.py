24aff48812579cafaf58d4cab11dc54aefb62062
aravindhshankar <aravindhswaminathan@gmail.com>
2024-10-08 22:40:01 +0200

    probably the right figure size now

--- manimfig/cover_try.py
+++ manimfig/cover_try.py
@@ -2,6 +2,24 @@ import numpy as np
 from functools import partial 
 from manim import *
 
+config.frame_width = 17.30 # units mm 
+config.frame_height = 24.60 # units mm
+config.pixel_width = int(config.frame_width * 100)
+config.pixel_height = int(config.frame_height * 100)
+
+
+class Hyperbolae(Scene):
+	def construct(self):
+		def impfun(x,y,a):
+			return x**2 - y**2 - a**2 
+		
+		titletext = Text('Strongly Correlated electrons in Sachdev-Ye-Kitaev models and \n Twisted Bilayer Graphene', color = BLUE).scale(1).align_on_border(np.array([0,1,0]),buff=0.5)
+		self.add(NumberPlane(), titletext)	
+		for aval in  np.array([1,2,3,4]):
+			graph = ImplicitFunction(partial(impfun, a = aval), color=YELLOW)
+			self.add(graph)
+
+
 class BraceAnnotation(Scene):
     def construct(self):
         dot = Dot([-2, -1, 0])
@@ -13,17 +31,6 @@ class BraceAnnotation(Scene):
         b2text = b2.get_tex("x-x_1")
         self.add(line, dot, dot2, b1, b2, b1text, b2text)
 
-
-class Hyperbolae(Scene):
-	def construct(self):
-		def impfun(x,y,a):
-			return x**2 - y**2 - a**2 
-		
-		titletext = Text('Strongly Correlated electrons in Sachdev-Ye-Kitaev models and \n Twisted Bilayer Graphene', color = BLUE).scale(1).align_on_border(np.array([0,1,0]),buff=0.5)
-		self.add(NumberPlane(), titletext)	
-		for aval in [1,2,3,4]:
-			graph = ImplicitFunction(partial(impfun, a = aval), color=YELLOW)
-			self.add(graph)
 
 
 
